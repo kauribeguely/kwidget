60297647e2b07649ddffa0c6a5a71c4773e19d2d
kauribeguely <kauri_che@hotmail.com>
2024-09-24 22:23:51 +1000

    Correct import of css

--- widgets/blogGrid.php
+++ widgets/blogGrid.php
@@ -1,7 +1,11 @@
 <?php
-namespace Elementor;
+// namespace Elementor;
 
-class Post_Grid_Widget extends Widget_Base {
+class Post_Grid_Widget extends \Elementor\Widget_Base {
+
+		public function get_style_depends() {
+			return [ 'blog-grid-style'];
+		}
 
     // Widget name
     public function get_name() {
@@ -64,23 +68,29 @@ class Post_Grid_Widget extends Widget_Base {
                 $featured_image = get_the_post_thumbnail_url( get_the_ID(), 'thumbnail' );  // Square image size
                 $post_title = get_the_title();
                 $post_link = get_permalink();
+								// Use excerpt if available, otherwise fall back to trimmed content
+								 $post_excerpt = has_excerpt() ? get_the_excerpt() : wp_trim_words( get_the_content(), 20, '...' );  // Limit to 20 words
 
-                // Render the post item
-                echo '<div class="post-item">';
-                echo '  <div class="post-thumbnail">';
-                echo '    <a href="' . esc_url( $post_link ) . '">';
-                echo '      <img src="' . esc_url( $featured_image ) . '" alt="' . esc_attr( $post_title ) . '" />';
-                echo '    </a>';
-                echo '  </div>';
-                echo '  <div class="post-content">';
-                echo '    <h3><a href="' . esc_url( $post_link ) . '">' . esc_html( $post_title ) . '</a></h3>';
-                echo '  </div>';
-                echo '</div>';
-            }
+								// Render the post item
+						    echo '<div class="post-item">';
 
-            echo '</div>';
-        }
+						    // Post thumbnail
+						    echo '  <div class="post-thumbnail">';
+						    echo '    <a href="' . esc_url( $post_link ) . '">';
+						    echo '      <img src="' . esc_url( $featured_image ) . '" alt="' . esc_attr( $post_title ) . '" />';
+						    echo '    </a>';
+						    echo '  </div>';
 
-        wp_reset_postdata();
-    }
+						    // Post content: title and excerpt
+						    echo '  <div class="post-content">';
+						    echo '    <h3><a href="' . esc_url( $post_link ) . '">' . esc_html( $post_title ) . '</a></h3>';
+						    echo '    <p>' . esc_html( $post_excerpt ) . '</p>';
+						    echo '  </div>';
+
+						    echo '</div>';  // End of post-item
+			        }
+
+			        wp_reset_postdata();
+			    }
+			}
 }
